614b5fac8644a17050093f66c67931fefe1a5175
abd alrhman abu salem <abusalemabdalrhman@gmail.com>
2025-07-15 15:33:35 +0300

    تعديلات على صفحة تسجيل الدخول والتسجيل وتصميم الواجهة

--- vite.config.js
+++ vite.config.js
@@ -7,8 +7,8 @@ export default defineConfig({
             input: [
                 'resources/css/app.css',
                 'resources/css/custom.css',
-                'resources/js/app.js',
-                'resources/js/custom.js'
+                'resources/js/app.js'
+            
             ],
             refresh: true,
         }),
